028d4575832f4e10ae78d4b5fbc95c56100398dc
TrueVale <srvleh@gmail.com>
2023-04-12 16:12:07 +0200

    [DEV-0001] Add image and synopsis to books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -46,6 +46,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :author)
+      params.require(:book).permit(:title, :author, :price, :image_url, :synopsis)
     end
 end
