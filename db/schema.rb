f7d8799917290b179cb6eded9f352ab285c35af1
TrueVale <srvleh@gmail.com>
2023-04-12 16:12:07 +0200

    [DEV-0001] Add image and synopsis to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_06_100700) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_141038) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
+    t.text "synopsis"
   end
 
 end
