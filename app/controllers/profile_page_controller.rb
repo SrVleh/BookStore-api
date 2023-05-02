a483f574f3b06f060a8b0c88106a35d3f679fca8
TrueVale <srvleh@gmail.com>
2023-05-02 15:56:43 +0200

    [DEV-0005] Uncontrolled changes

--- app/controllers/profile_page_controller.rb
+++ app/controllers/profile_page_controller.rb
@@ -1,13 +1,13 @@
 class ProfilePageController < ApplicationController
   before_action :authenticate_user!
-
   def show
     render json: {
       message: "This is a private message for #{current_user.email} you should only see if you've got a correct token",
       data: {
         email: current_user.email,
-        name: "HARDCODED TEST NAME!",
-        profile_pic: "HARDCODED PROFILE PICTURE!"
+        name: current_user.name,
+        username: current_user.username,
+        profile_pic: current_user.profile_pic
       }
     }
   end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, if: :devise_controller?
   respond_to :json
 
   private
@@ -55,9 +56,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+   def configure_sign_up_params
+     attributes = [:name, :username]
+     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+   end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
