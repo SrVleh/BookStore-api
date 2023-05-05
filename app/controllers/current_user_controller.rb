38a7f11cbb0836589a112328f451e4546b142335
TrueVale <srvleh@gmail.com>
2023-05-05 17:51:42 +0200

    [DEV-0006] Update user

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -1,6 +1,25 @@
 class CurrentUserController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_current_user, only: %i[ update ]
+
   def index
     render json: current_user, status: :ok
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_current_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :profile_pic)
   end
 end
