2b51b8cc047fee47bddff478804a571174410d49
TrueVale <srvleh@gmail.com>
2023-05-06 03:11:32 +0200

    [DEV-0007] Added admin field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_26_103047) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_06_003604) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_26_103047) do
     t.string "profile_pic"
     t.string "name"
     t.string "username"
+    t.boolean "isAdmin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
