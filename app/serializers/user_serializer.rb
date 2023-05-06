dcf665a933ffbd527503de7d8b14dabc3488278d
TrueVale <srvleh@gmail.com>
2023-05-06 03:11:32 +0200

    [DEV-0007] Added admin field

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :profile_pic, :name, :username, :created_at
+  attributes :id, :email, :profile_pic, :isAdmin, :name, :username, :created_at
 end
