c8afba394f2bce40509479540af4a8f70981e9a0
TrueVale <srvleh@gmail.com>
2023-06-15 22:09:02 +0200

    [DEV-0019] Remove stock column from books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_11_153527) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_15_200747) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_11_153527) do
     t.datetime "updated_at", null: false
     t.string "image_url"
     t.text "synopsis"
-    t.integer "stock", default: 1
     t.string "category"
   end
 
